fdf27a8f1b23a2baffcf64b1658a190ee3cdf1ae
shirley.x <shirleyx38@hotmail.com>
2023-12-03 21:32:34 +1300

    ColorCodeButtons, addition of fonts to lib

---
+++ colorpicker/src/routes/colorpicker/+page.svelte
@@ -0,0 +1,41 @@
+<script>
+	import ColorCodeButton from "./ColorCodeButton.svelte";
+	import Color from "../../../node_modules/colorjs.io"
+
+	let color = new Color("red");
+	color + ""; // default stringification
+	// let hex = color.to("p3").toString({precision: 3});
+	let sRGB = color.to("sRGB").toString();
+	console.log(color)
+	console.log(sRGB)
+
+</script>
+
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div style="
+	display: flex;
+	flex-direction: column;
+	color: white;
+">
+	<div style={`
+	background-color: ${color};
+	height: 20px;
+	width: 20px;`
+	}>
+
+	</div>
+	<ColorCodeButton header={"HEX"} value={color.to("sRGB").toString()} />
+	<ColorCodeButton header={"RGB"} value={color.to("sRGB").toString()} />
+	<ColorCodeButton header={"HSL"} value={color.to("HSL").toString()} />
+	<ColorCodeButton header={"HWB"} value={color.to("HWB").toString()} />
+
+	<ColorCodeButton header={"LAB"} value={color.to("Lab").toString()} />
+	<ColorCodeButton header={"LCH"} value={color.to("LCh").toString()} />
+
+	<ColorCodeButton header={"OKLAB"} value={color.to("OKLab").toString()} />
+	<ColorCodeButton header={"OKLCH"} value={color.to("OKLCh").toString()} />
+</div>
---
+++ colorpicker/src/routes/colorpicker/ColorCodeButton.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	export let header = "";
+	export let value = "";
+
+	const copyContent = async() => {
+		try {
+			await navigator.clipboard.writeText(value);
+		}
+		catch (err) {
+			console.error(err);
+		}
+	}
+</script>
+
+<div>
+	<h2>{header}</h2>
+	<button on:click={() => (copyContent())}>
+		{value}
+	</button>
+</div>
+
+
+<style>
+	div {
+		display: flex;
+		flex-direction: column;
+		gap: 4px;
+	}
+
+	h2 {
+		font-family: 'Montserrat-Bold';
+		color: #34126F;
+		font-size: 12px;
+	}
+
+	button {
+		border-radius: 27px;
+		height: 54px;
+		width: 300px;
+		background-color: #F8F5FC;
+		color: #34126F;
+
+		padding: 0;
+		border: none;
+		transition: box-shadow ease-in-out 300ms;
+
+		font-family: 'JetBrainsMono';
+	}
+
+	button:hover {
+		box-shadow: 0px 5px 5px #EAE2F8;
+	}
+</style>
