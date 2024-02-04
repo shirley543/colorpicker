951ad54b0e3a7325afd8b2dcf687be10ccacc2cc
shirley.x <shirleyx38@hotmail.com>
2024-02-04 14:32:39 +1300

    Add functions for converting rgb to hex, hex to rgb

--- colorpicker/src/routes/+page.svelte
+++ colorpicker/src/routes/+page.svelte
@@ -5,7 +5,7 @@
 	import Color from "../../node_modules/colorjs.io"
 	import './styles.css';
 
-	let color = new Color("red");
+	let color = new Color("pink");
 	$: sRGB = color.to("sRGB").toString();
 
 	function handleColorChange(inputColor: string) {
@@ -16,6 +16,42 @@
 		color = inputColor;
 	}
 
+	function convertRGBToHex(inputColor: Color) {
+		const r = inputColor.srgb.r * 255;
+		const g = inputColor.srgb.g * 255;
+		const b = inputColor.srgb.b * 255;
+		const a = inputColor.alpha;
+
+		function convertToHexPair(value: Number) {
+			return ('0' + value.toString(16)).slice(-2)
+		}
+
+		const hex = '#' + convertToHexPair(r) + convertToHexPair(g) + convertToHexPair(b) + convertToHexPair(a);
+		return hex
+	}
+
+	function convertHexToRGB(inputHex: string) {
+		// #RRGGBBAA
+		// Optional AA
+		const hex = inputHex.slice(1);
+		const rgb = parseInt(hex, 16);
+		if (hex.length === 8) {
+			const r = ((rgb & 0xFF000000) >> 32) / 255;
+			const g = ((rgb & 0x00FF0000) >> 16) / 255;
+			const b = ((rgb & 0x0000FF00) >> 8) / 255;
+			const a = ((rgb & 0x000000FF) >> 0) / 255;
+
+			const output = new Color({ space: "sRGB", coords: [r, g, b], alpha: a });
+			return output;
+		} else if (hex.length === 6) {
+			const r = ((rgb & 0xFF0000) >> 16) / 255;
+			const g = ((rgb & 0x00FF00) >> 8) / 255;
+			const b = ((rgb & 0x0000FF) >> 0) / 255;
+
+			const output = new Color("sRGB", [r, g, b]);
+			return output;
+		}
+	}
 </script>
 
 <svelte:head>
@@ -51,7 +87,7 @@
 	">
 		<ColorCodeInput handleEnter={handleColorChange}/>
 	
-		<ColorCodeButton header={"HEX"} value={color.to("sRGB").toString()} />
+		<ColorCodeButton header={"HEX"} value={convertRGBToHex(color)} />
 		<ColorCodeButton header={"RGB"} value={color.to("sRGB").toString()} />
 		<ColorCodeButton header={"HSL"} value={color.to("HSL").toString()} />
 		<ColorCodeButton header={"HWB"} value={color.to("HWB").toString()} />
